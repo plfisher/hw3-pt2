9146a105340e065281c5dc17e92462102879f7c3
plfisher <47302771+plfisher@users.noreply.github.com>
2019-03-27 15:34:44 -0400

    Update prog1.c

--- prog1.c
+++ prog1.c
@@ -6,20 +6,34 @@
 int main(int argc, char** argv) {
 int t1 = 0, t2 = 1, nextTerm = 0, n;
   int i=0;
+  int result;
   
   char * numchar=(*(argv+1));
   int num=atoi(numchar);
-  fork();
+  n=num;
+  pid=Fork();
+  if(pid==0)
+  {
   for (i = 1; i <= n; ++i)
     {
         nextTerm = t1 + t2;
         t1 = t2;
         t2 = nextTerm;
     }
-  
+   result=t2%10;
+  }
   if (fork() == 0) 
   {
     printf("%d", t2%10);
   }
   
+}
+
+pid_t Fork(void)
+{
+  pid_t pid;
+  
+  if((pid=fork())<0)
+    unix_error("Fork error");
+  return pid;
 }
