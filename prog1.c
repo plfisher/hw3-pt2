abccff17ae1968047a0e27b4599ecf42478435f0
plfisher <47302771+plfisher@users.noreply.github.com>
2019-03-26 23:02:41 -0400

    Update prog1.c

--- prog1.c
+++ prog1.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(int argc, char** argv) {
+#include <sys/types.h> 
+#include <unistd.h>
 
+int main(int argc, char** argv) {
+int t1 = 0, t2 = 1, nextTerm = 0, n;
+  
   char * numchar=(*(argv+1));
   int num=atoi(numchar);
-  printf("%d", num);
+  fork();
+  for (i = 1; i <= n; ++i)
+    {
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+    }
+  
+  if (fork() == 0) 
+  {
+    printf("%d", t2%10)
+  }
   
 }
