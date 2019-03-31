2b2116e2162657ec525c05c8addf879e438b95a5
plfisher <47302771+plfisher@users.noreply.github.com>
2019-03-30 22:56:33 -0400

    Update prog1.c

--- prog1.c
+++ prog1.c
@@ -2,29 +2,29 @@
 #include <stdlib.h>
 #include <sys/types.h> 
 #include <unistd.h>
+#include <math.h>
 
 int main(int argc, char** argv) {
-int t1 = 0, t2 = 1, nextTerm = 0, n;
-  int i=0;
-  int result;
+
   
   char * numchar=(*(argv+1));
   int num=atoi(numchar);
   n=num;
-  pid=Fork();
+  
+  pid_t pid=Fork();
   if(pid==0)
   {
-  for (i = 1; i <= n; ++i)
-    {
-        nextTerm = t1 + t2;
-        t1 = t2;
-        t2 = nextTerm;
-    }
-   result=t2%10;
+  int gnum=(1+sqrt(5))/2;
+    int numerator=(pow(gnum, num))-(pow((1-gnum), num));
+    int result=round(numerator/sqrt(5));
+    exit(result);
   }
- 
-  
-}
+  else
+  {
+    signal(SIGCHLD, handler);
+    int returnval=WIFEXITED(pid);
+    printf("%d", returnval);
+  }
 
 pid_t Fork(void)
 {
@@ -33,4 +33,10 @@ pid_t Fork(void)
   if((pid=fork())<0)
     unix_error("Fork error");
   return pid;
+}
+  
+void handler(int sig)
+{
+  pid_t pid;
+  pid=wait(NULL);
 }
