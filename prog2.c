474e0999a80db1bbfaaaf727e5a1895d2053a401
plfisher <47302771+plfisher@users.noreply.github.com>
2019-03-31 22:49:02 -0400

    Update prog2.c

--- prog2.c
+++ prog2.c
@@ -1,1 +1,23 @@
 
+int main(int argc, char **argv)
+{
+  lsh_loop();
+}
+
+void lsh_loop(void)
+{
+  char *line;
+  char **args;
+  int status;
+
+  do {
+    printf("shell> ");
+    line = lsh_read_line();
+    args = lsh_split_line(line);
+    status = lsh_execute(args);
+
+    free(line);
+    free(args);
+  } while (status);
+}
+
