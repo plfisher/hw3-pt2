5e8286b3f54592397f4fe1fe83033cd2c831b897
plfisher <47302771+plfisher@users.noreply.github.com>
2019-04-01 23:27:21 -0400

    Update prog2.c

--- prog2.c
+++ prog2.c
@@ -99,9 +99,9 @@ int shell_help(char **args);
 int shell_exit(char **args);
 
 char *builtincommands[3];
-builintcommands[0]="cd";
-builintcommands[1]="help";
-builintcommands[2]="exit";
+builtincommands[0]="cd";
+builtincommands[1]="help";
+builtincommands[2]="exit";
 
 int shellbuiltin=3;
 int(*builtincommands[]) (char**)={
