3a659b434b28f5e1754ea29d5fbf75ca544b996c
plfisher <47302771+plfisher@users.noreply.github.com>
2019-04-01 23:20:36 -0400

    Update prog2.c

--- prog2.c
+++ prog2.c
@@ -1,7 +1,16 @@
+#define LSH_RL_BUFSIZE 1024
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <string.h>
 
 int main(int argc, char **argv)
 {
   lsh_loop();
+  
+  
 }
 
 void lsh_loop(void)
@@ -21,3 +30,122 @@ void lsh_loop(void)
   } while (status);
 }
 
+char *lsh_read_line(void)
+{
+  char *line = NULL;
+  ssize_t bufsize = 0; // have getline allocate a buffer for us
+  getline(&line, &bufsize, stdin);
+  return line;
+}
+char **lsh_split_line(char *line)
+{
+  int buffersize=64;
+  int i=0;
+  char **args=malloc(buffersize*sizeof(char*));
+  char *arg;
+  
+  arg=strok(line, " \t\r\n\a");
+  while(arg!=NULL)
+  {
+    args[i]=arg;
+    i++;
+    
+    if(i>= buffersize)
+    {
+      bufsize+=64;
+      args=realloc(args, buffersize *sizeof(char*));
+      
+    }
+     args=strok(NULL, " \t\r\n\a");
+  }
+ args[i]=NULL;
+ return args; 
+}
+
+int lsh_launch(char **args)
+{
+  pid_t pid, wpid;
+  int status;
+
+  pid = fork();
+  if (pid == 0) {
+    // Child process
+    if (execvp(args[0], args) == -1) {
+      perror("lsh");
+    }
+    exit(EXIT_FAILURE);
+  } else if (pid < 0) {
+    // Error forking
+    perror("lsh");
+  } else {
+    // Parent process
+    do {
+      wpid = waitpid(pid, &status, WUNTRACED);
+    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+  }
+
+  return 1;
+}
+
+int lsh_cd(char **args);
+int lsh_help(char **args);
+int lsh_exit(char **args);
+
+char *builtincommands[3];
+*builintcommands[0]="cd";
+*builintcommands[1]="help";
+*builintcommands[2]="exit";
+
+int shellbuiltin=3;
+int(*builtincommands[]) (char**)={
+  &shell_cd,
+  &shell_help,
+  &shell_exit
+};
+
+int shell_cd(char **args)
+{
+  if(args[1]==NULL)
+  {
+    fprintf(stderr, "shell: expected argument to \"cd\"\n");
+  }
+  else {
+    if(chdir(args[1])!=0)
+    {
+      perror("lsh");
+    }
+  }
+  return 1;
+}
+
+int shell_help(char **args)
+{
+  int i;
+  printf("Type a command to be executed");
+  return 1;
+}
+
+int shell_ext(char **args)
+{
+  return 0;
+}
+
+
+int lsh_execute(char **args)
+{
+  int i;
+  
+  if(args[0]==NULL)
+  {
+    return 1;
+  }
+  
+  for(i =0;i<shellbuiltin;i++)
+  {
+    if(strcmp(args[0], builtincommands[i])==0)
+    {
+      return(*builtincommands[i])(args);
+    }
+  }
+  return lsh_launch(args);
+}
